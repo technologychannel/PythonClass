fc54e38e255dcf8d0d578a658cac40081b89b2cf
Bishworaj Poudel <bishworajpoudelofficial@gmail.com>
2024-06-21 22:49:36 +0545

    Code Added

---
+++ 25type_convert.py
@@ -0,0 +1,15 @@
+number1 = input("Enter Number 1: ")
+number2 = input("Enter Number 2: ")
+
+
+
+sum = int(number1) + int(number2)
+
+print(f"Total is {sum}")
+
+
+### Create a program, that convert Nepali Currency to USD, Euro, Japnease
+### Enter Amount in NRS: 5000
+### USD is: $45
+### Euro is: $43
+### Japnease amount is: 8454
